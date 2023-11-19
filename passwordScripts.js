63bed32a8dda02c9daaeb45ab5988b4b4fe77a6b
ASpacefish <f015232h@student.staffs.ac.uk>
2023-11-19 16:24:55 +0000

    Images and Link changes
    - Added images to folder and implemented them in html and js code.
    
    -Changed functionality from QR codes to auto opening links on correct password guess.

--- passwordScripts.js
+++ passwordScripts.js
@@ -6,16 +6,17 @@ function myFunction(event) {
     .then(urlResponse => urlResponse.json())
     .then(json => json.forEach(element => {
         if (element.password == document.getElementById("password").value.toLowerCase()) {
-            console.log(element.qrCode)
+            console.log(element.ytlink)
             correctChecker = 1
-            document.getElementById("mainImage").src = element.qrCode
+            window.open(element.ytlink);
+            document.getElementById("mainImage").src = "imgs/treasure_open.png"
             document.getElementById("confirmText").innerHTML = element.comment
         }    
 
     })).then( function finalfunciton() {
         if (correctChecker == 0) {
             document.getElementById("confirmText").innerHTML = "Damn, not quite the right password :("
-            document.getElementById("mainImage").src = "https://b.l3n.co/i/Bpzqte.png"
+            document.getElementById("mainImage").src = "imgs/treasure_closed.png"
         }} );
 }
 
